47aeda9f87f6570fc1d4c3339dd9ca648ae73562
Shreyas <92593919+ShreyasDJ2001@users.noreply.github.com>
2023-05-29 10:25:04 -0700

    Add files via upload
    first-commit

---
+++ WebApplication2/Controller/HelloController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controller
+{
+    [ApiController]
+    [Route("/[Controller]")]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok("Hello Worldd");
+        }
+    }
+}
